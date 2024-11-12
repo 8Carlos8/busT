1b3147f465826d86d356c274eb5dea76e8a58022
nickysaavedra25 <157611273+nickysaavedra25@users.noreply.github.com>
2024-11-12 14:22:59 -0600

    cambio paginaweb

---
+++ resources/views/pagina-web.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADO</title>
+    <link rel="stylesheet" href="../resources/css/app.css">
+</head>
+<body>
+<a href="https://api.whatsapp.com/send?phone=2481103306" class="btn-wsp" target="_blank">
+    <i class="fa fa-whatsapp icono"></i>
+</a>
+
+<header class="header" data-header>
+  <div class="container">
+    <button class="nav-open-btn" aria-label="open menu" data-nav-toggler>
+      <ion-icon name="menu-outline" aria-hidden="true"></ion-icon>
+    </button>
+
+    <a href="#" class="logo">
+      <img src="" width="100" height="60" alt="Logo" class="logo-light">
+    </a>
+
+    <nav class="navbar" data-navbar>
+      <div class="navbar-top">
+        <a href="#" class="logo">
+          <img src="" width="100" height="60" alt="logo">
+        </a>
+
+        <button class="nav-close-btn" aria-label="close menu" data-nav-toggler>
+          <ion-icon name="close-outline" aria-hidden="true"></ion-icon>
+        </button>
+      </div>
+
+        <ul class="navbar-list">
+          <li><a href="#inicio" class="navbar-link">INICIO</a></li>
+          <li><a href="#servicios" class="navbar-link">SERVICIOS</a></li>
+          <li><a href="#contacto" class="navbar-link">CONTACTO</a></li>
+          <li><a href="#login" class="navbar-link">INICIAR SESIÓN</a></li>  
+        <ul class="social-list">
+
+    
+        </ul>
+
+      </nav>
+      <div class="overlay" data-nav-toggler data-overlay></div>
+    </div>
+  </header>
+
+    
+</body>
+</html>
