2322b4827fb602d78e197f7d47e64785ba686090
nickysaavedra25 <157611273+nickysaavedra25@users.noreply.github.com>
2024-11-13 14:25:08 -0600

    hey

--- resources/views/pagina-web.php
+++ resources/views/pagina-web.php
@@ -1,13 +1,29 @@
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ADO</title>
-    <link rel="stylesheet" href="../resources/css/app.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Somos un grupo de expertos en el sector inmobiliario que se preocupan y trabajan para ti.">
+  <title>ADO</title>
+  <meta name="icon" href="ADO-Autobuses-de-Oriente-Logo-700x394.png">
+  <link rel="icon" href="ADO-Autobuses-de-Oriente-Logo-700x394.png" type="image/x-icon">
+  <link rel="shortcut icon" href="ADO-Autobuses-de-Oriente-Logo-700x394.png" type="image/x-icon">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <link rel="preload" as="image" href="images/hero-slide-1.jpg">
+  <link rel="preload" as="image" href="images/hero-slide-2.jpg">
+  <link rel="preload" as="image" href="images/hero-slide-3.jpg"><script src="https://kit.fontawesome.com/b408879b64.js" crossorigin="anonymous"></script>
+  <meta name="google-site-verification" content="PrXlD6g0ZZC_xq4kjb79ZmXNiFxZVGFKqhMcMgGgI24" />
+  <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
-<a href="https://api.whatsapp.com/send?phone=2481103306" class="btn-wsp" target="_blank">
+  <a href="https://api.whatsapp.com/send?phone=2481103306" class="btn-wsp" target="_blank">
     <i class="fa fa-whatsapp icono"></i>
 </a>
 
@@ -18,13 +34,13 @@
     </button>
 
     <a href="#" class="logo">
-      <img src="" width="100" height="60" alt="Logo" class="logo-light">
+      <img src="ADO-Autobuses-de-Oriente-Logo-700x394.png" width="100" height="60" alt="Logo" class="logo-light">
     </a>
 
     <nav class="navbar" data-navbar>
       <div class="navbar-top">
         <a href="#" class="logo">
-          <img src="" width="100" height="60" alt="logo">
+          <img src="./ADO-Autobuses-de-Oriente-Logo-700x394.png" width="100" height="60" alt="Adex home">
         </a>
 
         <button class="nav-close-btn" aria-label="close menu" data-nav-toggler>
@@ -33,59 +49,235 @@
       </div>
 
         <ul class="navbar-list">
-          <li><a href="#inicio" class="navbar-link">INICIO</a></li>
-          <li><a href="#servicios" class="navbar-link">SERVICIOS</a></li>
-          <li><a href="#contacto" class="navbar-link">CONTACTO</a></li>
-          <li><a href="#login" class="navbar-link">INICIAR SESIÓN</a></li>  
+          <li><a href="#inicio" class="navbar-link">NOSOTROS</a></li> <li><a href="#ubicacion" class="navbar-link">UBICACIÓN</a></li>
+          <li><a href="#contacto" class="navbar-link">SOPORTE</a></li>  
+          <li><a href="#login" class="navbar-link">INICIAR SESIÓN</a></li>
+      
+
         <ul class="social-list">
 
     
         </ul>
 
       </nav>
+
+      
+
+
       <div class="overlay" data-nav-toggler data-overlay></div>
+
     </div>
   </header>
-  <div class="caja" id="contacto">
-        <div class="box-info">
-            <h1>CONTÁCTO</h1>
-          
-            <div class="data">
-                <p><i class="fa-solid fa-envelope"></i>CORREO</p>
-                <p><i class="fa-solid fa-location-dot"></i>DIRECCIONES</p>
-            </div>
-            <div class="links">
-                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                <a href="#"><i class="fa-brands fa-instagram"></i></a>
-        
+ 
+
+<script>
+const menuIcon = document.querySelector('.menu-icon');
+const nav = document.querySelector('nav');
+
+menuIcon.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+</script>
+
+
+<div class="carousel">
+
+    <div class="list">
+
+        <div class="item" style="background-image: url(./IMG_4967.JPG);">
+            <div class="content">
+              <div class="title"></div>
+              <div class="name"></div>
+              <div class="des"></div>
+              <div class="btn">
+                </div>
             </div>
         </div>
-        <form action="https://formsubmit.co/creacode0@gmail.com" method="POST">
-            <div class="input-box">
-                <input type="text" id="name"class="name" placeholder="Nombre" required>
-                <i class="fa-solid fa-user"></i>
+
+        <div class="item" style="background-image: url(./128050514_10157878327480547_4040559758234544922_o.jpg);">
+            
+            <div class="content">
+              <div class="title"></div>
+              <div class="name"></div>
+              <div class="des"></div>
+              <div class="btn">
+                </div>
             </div>
-            <div class="input-box">
-              <input type="text" id="mail"class="mail"required placeholder="Correo">
-                <i class="fa-solid fa-envelope"></i>
+
+        </div>
+
+        <div class="item" style="background-image: url(./19.jpg);">
+
+            <div class="content">
+              <div class="title"></div>
+              <div class="name"></div>
+              <div class="des"></div>
+              <div class="btn">
+                </div>
             </div>
-            <div class="input-box">
-              <input type="text" id="phone"class="phone"placeholder="Celular">
-                <i class="fa-solid fa-pen-to-square"></i>
+
+        </div>
+
+        <div class="item" style="background-image: url(./ado_buses.jpeg);">
+            
+            <div class="content">
+              <div class="title"></div>
+              <div class="name"></div>
+              <div class="des"></div>
+              <div class="btn">
+                </div>
             </div>
-            <div class="input-box">
-              <textarea name="message" class="message" placeholder="Escribe tu mensaje..."></textarea>
-            </div>
-            <button type="submit">Enviar mensaje</button>
-        </form>
+
+        </div>
+
+   
+
+        </div>
+
+       
+        </div>
+
+    </div>
+
+    <!--next prev button-->
+    <div class="arrows">
+        <button class="prev"><</button>
+        <button class="next">></button>
     </div>
 
 
-        <section class="mapa" id="ubicacion">
-          <iframe src=""></iframe>
-   
+    <!-- time running -->
+    <div class="timeRunning"></div>
 
-      </section>
-    
+</div>
+  <section id="nosotros" class="nosotros-section">
+    <div class="container">
+      <h2 class="section-title">NOSOTROS</h2>
+      <div class="content-wrapper">
+        <div class="text-content">
+          <p>
+            Desde nuestros inicios en 1939, ADO ha sido pionero en el transporte terrestre, conectando ciudades y comunidades en todo México. Fundada con la visión de ofrecer servicios de transporte seguros y confiables, ADO ha evolucionado para convertirse en un referente en la industria, caracterizada por la innovación, la calidad y el compromiso hacia nuestros pasajeros.
+          </p>
+          <p>
+            A lo largo de los años, hemos ampliado nuestras rutas, mejorado nuestras flotas y elevado el estándar en el servicio al cliente. Hoy en día, nuestra red conecta personas a lo largo y ancho del país, con un enfoque en la comodidad, la seguridad y la sostenibilidad.
+          </p>
+        </div>
+        <div class="image-content">
+          <img src="./1.jpg" alt="Imagen histórica de autobuses ADO" class="img-cover">
+        </div>
+      </div>
+      <div class="values-section">
+        <h2 class="section-title">Misión</h2>
+        <p>
+          Nuestra misión es brindar un servicio de transporte terrestre de alta calidad, enfocado en la seguridad, comodidad y accesibilidad de nuestros pasajeros. Buscamos ser una parte integral de la vida cotidiana de millones de personas, conectando comunidades, familias y empresas en todo México, mediante una infraestructura moderna y eficiente. Nos comprometemos a ofrecer una experiencia de viaje que inspire confianza, bienestar y satisfacción en cada uno de nuestros usuarios.
+        </p>
+        <p>
+          A través de la innovación constante, el uso de tecnología avanzada y un equipo de trabajo capacitado, ADO no solo transporta personas, sino que también es una empresa que promueve la sostenibilidad, el respeto al medio ambiente y la mejora continua en todos nuestros procesos operativos.
+        </p>
+        
+        <h2 class="section-title">Visión</h2>
+        <p>
+          Nuestra visión es consolidarnos como la empresa líder en transporte terrestre en México, ofreciendo soluciones de movilidad que superen las expectativas de nuestros clientes. Queremos ser reconocidos por nuestra capacidad para adaptarnos a los nuevos retos del transporte, integrando nuevas tecnologías y optimizando nuestras operaciones para brindar un servicio de clase mundial.
+        </p>
+        <p>
+          En ADO, buscamos estar a la vanguardia de la industria, ampliando constantemente nuestra red de rutas, innovando en nuestros servicios y fortaleciendo nuestro compromiso con el bienestar social y la sostenibilidad. Queremos que nuestros pasajeros confíen plenamente en nosotros como su principal opción para viajar de manera segura, cómoda y eficiente, siendo parte activa del desarrollo económico y social del país.
+        </p>
+        <h2 class="section-title">Valores</h2>
+        <ul>
+          <li align="center">Seguridad</li>
+          <li align="center">Innovación</li>
+          <li align="center">Calidad</li>
+          <li align="center">Compromiso</li>
+          <li align="center">Sostenibilidad</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  
+  <footer class="footer">
+    <div class="container grid-list">
+
+      <div class="footer-brand">
+
+        <a href="#" class="logo">
+          <img src="./ADO-Autobuses-de-Oriente-Logo-700x394.png" width="100" height="50" alt="Adex home">
+        </a>
+
+        <p class="footer-text">
+          &copy; 2024 ADO. <br> Todos los derechos reservados.
+        </p>
+
+        <ul class="social-list">
+
+          <li>
+            <a href="https://www.facebook.com/share/Mv3o7WsSgzNGN1EG/?mibextid=JRoKGi"
+            " class="social-link">
+              <ion-icon name="logo-facebook"></ion-icon>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/casa_tierra_inmobiliaria/ "class=social-link">
+              <ion-icon name="logo-instagram"></ion-icon>
+            </a>
+          </li>
+
+
+        </ul>
+
+      </div>
+
+      <ul class="footer-list">
+
+        <li>
+          <p class="h4 footer-list-title">Nosotros</p>
+        </li>
+
+        <li>
+          <address class="footer-text">
+            Acerca De
+          </address>
+        </li>
+
+       
+      </ul>
+
+      <ul class="footer-list">
+
+        <li>
+          <p class="h4 footer-list-title">Contacto</p>
+        </li>
+
+        <li>
+          <a href="#contacto" class="footer-link">Telefono</a>
+        </li>
+        <li>
+          <a href="#contacto" class="footer-link">Correo</a>
+        </li>
+
+
+      </ul>
+
+      
+  </footer>
+
+
+
+
+  <script
+  type="module"
+  src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+></script>
+<script
+  nomodule
+  src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+></script>
+<script src="./js/script.js"></script>
+
+  <script src="js/script.js"></script>
+
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
 </body>
+
 </html>
